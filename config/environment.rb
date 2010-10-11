4fb1408094bdd02a67865f06428bb8e224063b99
Fernando Gomes <feromes@gmail.com>
2010-10-11 10:49:08 -0300

    Google Application ID

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Entregas::Application.initialize!
 
-GOOGLE_APPLICATION_ID = "ABQIAAAAyQzxdePRSK3_34DgQe74qBTDbpLS7Ko3u9LHK0Rk-VPjI4JCQxSb8a_GRmaj0Wu7SMRrTbGqaf-KRg"
+GOOGLE_APPLICATION_ID = "ABQIAAAAyQzxdePRSK3_34DgQe74qBS1-0SvkuRKjUbttSQzRsTCobn2YhT2GFSiGhSvuyo9vpD0PveojBLNnA"
