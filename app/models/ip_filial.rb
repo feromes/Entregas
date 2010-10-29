65f77ef2bb73601ba2b0f898504ad7715bfe83c7
Fernando Gomes <feromes@gmail.com>
2010-10-29 08:33:43 -0200

    Ips das filiais

--- app/models/ip_filial.rb
+++ app/models/ip_filial.rb
@@ -4,8 +4,16 @@ class IpFilial
     
   FILIAIS = {
     IPAddr.new("192.168.6.0/24") => 13,
-    IPAddr.new("192.168.1.0/24") => 1
-    
+    IPAddr.new("192.168.0.0/24") => 1,
+    IPAddr.new("192.168.3.0/24") => 2,
+    IPAddr.new("192.168.4.0/24") => 4,
+    IPAddr.new("192.168.10.0/24") => 21,
+    IPAddr.new("192.168.1.0/24") => 15,
+    IPAddr.new("192.168.8.0/24") => 8,
+    IPAddr.new("192.168.7.0/24") => 10,
+    IPAddr.new("192.168.5.0/24") => 19,
+    IPAddr.new("192.168.9.0/24") => 9,
+    IPAddr.new("192.168.2.0/24") => 3,
   }
   
   attr :filial
